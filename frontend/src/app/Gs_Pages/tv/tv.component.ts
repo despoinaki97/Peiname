11afd20c272714d6e5c5dfed69179e8c4256408b
Despoina Gavgiotaki <despoinaki97@gmail.com>
2019-12-10 14:36:41 +0200

    scroll on tv with leap

--- frontend/src/app/Gs_Pages/tv/tv.component.ts
+++ frontend/src/app/Gs_Pages/tv/tv.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ɵɵresolveBody } from '@angular/core';
+import { LeapService,Gestures} from 'src/app/cursor/leap.service';
 @Component({
   selector: 'ami-fullstack-tv',
   templateUrl: './tv.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _leapService:LeapService) { }
 
   ngOnInit() {
+    this._leapService.gestureRecognizer().subscribe((gesture)=>{
+      if(gesture == Gestures.CIRCLE_CLOCKWISE){
+        console.log('swipe up tv');
+        document.getElementById('scroll').scrollTo(0, 100);
+        document.getElementById('Welcome').style.color='red';
+      }
+    });
   }
 
 }
--- frontend/src/app/pages/choosecuisine/choosecuisine.component.ts
+++ frontend/src/app/pages/choosecuisine/choosecuisine.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabankService } from 'src/app/databank.service';
 import { Router } from '@angular/router';
+import { LeapService ,Gestures} from 'src/app/cursor/leap.service';
 
 @Component({
   selector: 'ami-fullstack-choosecuisine',
@@ -9,14 +10,21 @@ import { Router } from '@angular/router';
 })
 export class ChoosecuisineComponent implements OnInit {
 
-  constructor( private DatabankService: DatabankService,private router:Router) { }
+  constructor( private DatabankService: DatabankService,private router:Router,private _leapService:LeapService) { }
 
   callServer(){
     this.DatabankService.call('vote_done',localStorage.getItem("seat"));
     this.router.navigateByUrl("/stateofcuisine");
   }
 
-  ngOnInit() {
-  }
 
+  ngOnInit() {
+    // this._leapService.gestureRecognizer().subscribe((gesture)=>{
+    //   if(gesture == Gestures.SWIPE_UP){
+    //     console.log('swipe up cuis');
+    //     document.getElementById('scroll').scrollTo(0, 40);
+
+    //         }
+    // });
+  }
 }
