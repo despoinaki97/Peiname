3fa93c4fe1dd39dd4f7f701c9a55e2f593379a55
Despoina Gavgiotaki <despoinaki97@gmail.com>
2019-12-12 14:36:56 +0200

    fixes

--- frontend/src/app/rating-shop-list/rating-shop-list.component.ts
+++ frontend/src/app/rating-shop-list/rating-shop-list.component.ts
@@ -3,6 +3,7 @@ import { Shop } from '../shop';
 import { Rating } from '../rating';
 import { ordAccount } from '../ordaccount';
 import { DatabankService } from '../databank.service';
+import { LeapService,Gestures} from 'src/app/cursor/leap.service';
 
 
 @Component({
@@ -13,14 +14,28 @@ import { DatabankService } from '../databank.service';
 export class RatingShopListComponent implements OnInit {
   users: ordAccount[];
   Shops;
-  constructor(private databank:DatabankService) {
+  constructor(private databank:DatabankService,private _leapService:LeapService) {
    
   }
 
   ngOnInit() {
     this.databank.getUsers().subscribe((users)=>{
       this.users = users;
+
       
+    });
+
+    var sub = this._leapService.gestureRecognizer().subscribe((gesture)=>{
+      if(gesture == Gestures.SWIPE_RIGHT){
+        console.log('swipe left tv');
+        document.getElementById('horscroll').scrollTo(1000, 0);
+    //    document.getElementById('Welcome').style.color='blue';
+      } if(gesture == Gestures.SWIPE_LEFT){
+        console.log('swipe left tv');
+        document.getElementById('horscroll').scrollTo(-1000, 0);
+    //    document.getElementById('Welcome').style.color='blue';
+      }
+   
     });
 
     console.log(this.users);
@@ -57,7 +72,38 @@ export class RatingShopListComponent implements OnInit {
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
   
       ]
-      )    
+      )    ,
+      
+      new Shop(0,"Pita tou Papou",5.0,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+        new Rating(0,this.users[1],"Good, but late!",4.0),
+        // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
+  
+      ]
+      )    ,
+      
+      new Shop(0,"Pita tou Papou",5.0,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+        new Rating(0,this.users[1],"Good, but late!",4.0),
+        // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
+  
+      ]
+      )    ,
+      
+      new Shop(0,"Pita tou Papou",5.0,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+        new Rating(0,this.users[1],"Good, but late!",4.0),
+        // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
+  
+      ]
+      )    ,
+      new Shop(0,"Pita tou Papou",5.0,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+        new Rating(0,this.users[1],"Good, but late!",4.0),
+        // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
+  
+      ]
+      )    ,
     ]
 
 
