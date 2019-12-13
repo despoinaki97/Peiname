7f819d4f245a789ec2b3ceaa7497174c9ecc4984
Despoina Gavgiotaki <despoinaki97@gmail.com>
2019-12-13 16:41:11 +0200

    fixed wall ratings

--- frontend/src/app/rating-shop-list/rating-shop-list.component.ts
+++ frontend/src/app/rating-shop-list/rating-shop-list.component.ts
@@ -42,67 +42,69 @@ export class RatingShopListComponent implements OnInit {
 
 
     this.Shops = [
-      new Shop(0,"Gyrogwnia",4.0,[
+      new Shop(0,"Gyrogonia",4.3,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+        new Rating(0,this.users[1],"Good, but late!",4.0),
+        new Rating(0,this.users[0],"Μεγάλη ποικιλία :) ",4)
+      ],"","","../../../assets/images/Rectangle 2.2 (3).png"
+      ),
+      new Shop(0,"Krasas",4.1,[
+        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
+  
+      ],"","", "../../../assets/krassas.webp"
+      ),
+      new Shop(0,"Xovoli",4.0,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
   
-      ]
+      ],"","","../../../assets/images/Rectangle 2.2 (4).png"
       ),
-      new Shop(0,"KRasas",2.0,[
-        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
-  
-      ]
-      ),
-      new Shop(0,"Xobolh",4.0,[
+      new Shop(0,"Ta aderfia",5.0,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
-  
-      ]
-      ),
-      new Shop(0,"Tu sifaliou th taverna",5.0,[
-        new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
-        new Rating(0,this.users[1],"Good, but late!",4.0),
-  
-      ]
+        new Rating(0,this.users[2],"Πολύ καλο και γρήγορο",5.0),
+
+      ],"","","./../../assets/aderfia.webp"
       ),
   
-      new Shop(0,"Pita tou Papou",5.0,[
+      new Shop(0,"Pita tou Papou",4.6,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
   
-      ]
+      ],"","","./../../assets/images/Rectangle 2.2 (2).png"
       )    ,
       
-      new Shop(0,"Pita tou Papou",5.0,[
+      new Shop(0,"Magevmata",4.3,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
   
-      ]
+      ],"","","../../../assets/images/Rectangle 2.2 (1).png"
       )    ,
       
-      new Shop(0,"Pita tou Papou",5.0,[
+      new Shop(0,"Pansetomprizoladiko",4.5,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
   
-      ]
+      ],"","","./../../assets/panse.webp"
       )    ,
       
-      new Shop(0,"Pita tou Papou",5.0,[
+      new Shop(0,"Ntelogo",3.9,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
-  
-      ]
+        new Rating(0,this.users[2],"Πολύ καλο και γρήγορο",5.0),
+
+      ],"","","./../../assets/ntelogo.webp"
       )    ,
-      new Shop(0,"Pita tou Papou",5.0,[
+      new Shop(0,"Plateia",5.0,[
         new Rating(0,this.users[0],"Πολύ καλο και γρήγορο",5.0),
         new Rating(0,this.users[1],"Good, but late!",4.0),
         // new Rating(0,new ordAccount("Despoina.G"),"Κακο",2.0) // cannot support *.5
   
-      ]
+      ],"","","./../../assets/πλατεια.webp"
       )    ,
     ]
 
